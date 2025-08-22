76ff473d549f1858cb0ab22db1de2d540289c190
Lex Mel <lex07672@gmail.com>
2025-08-22 14:55:26 +0300

    feat: add .prettierrc, add correct settings for eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,27 @@
 import js from "@eslint/js";
 
 export default [
-  js.configs.recommended, // стандартные правила ESLint
   {
+    ignores: ["old-version/**"], // теперь игнорируется (раньше файлом .eslintignore)
+
+    languageOptions: {
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        navigator: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly"
+      }
+    },
+
     rules: {
-      "no-unused-vars": "warn", 
+      "no-unused-vars": "warn",
       "no-console": "off"
-    }
+    },
+
+    // включаем правила recommended через spread. для игнора old-version/ записано снизу
+    ...js.configs.recommended
   }
 ];
