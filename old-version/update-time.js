5e51c2474f0911bf4deb888e0d969dc304d7aa0b
Lex Mel <lex07672@gmail.com>
2025-08-22 13:48:06 +0300

    feat: add scripts lint and format, run format

--- old-version/update-time.js
+++ old-version/update-time.js
@@ -1,12 +1,12 @@
- // Функция для обновления даты последнего изменения
- function updateLastUpdatedDate() {
-    let lastUpdatedElement = document.getElementById("site-update");
-    let lastUpdatedDate = new Date(document.lastModified);
-    lastUpdatedElement.textContent = lastUpdatedDate.toLocaleString();
+// Функция для обновления даты последнего изменения
+function updateLastUpdatedDate() {
+  let lastUpdatedElement = document.getElementById("site-update");
+  let lastUpdatedDate = new Date(document.lastModified);
+  lastUpdatedElement.textContent = lastUpdatedDate.toLocaleString();
 }
 
 // Обновляем дату сразу после загрузки страницы
 updateLastUpdatedDate();
 
 // Обновляем дату каждую минуту (можете настроить интервал под свои нужды)
- setInterval(updateLastUpdatedDate, 60000);
+setInterval(updateLastUpdatedDate, 60000);
--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function copyText(event) {
   // Находим текущую кнопку по элементу, на который нажали
   const button = event.target;
-  
+
   // Создаём элемент textarea в памяти, чтобы поместить в него текст кнопки
   const textarea = document.createElement("textarea");
   textarea.value = button.innerText;
-  
+
   // Добавляем элемент textarea на страницу (но не видимый для пользователя)
   document.body.appendChild(textarea);
-  
+
   // Выделяем текст внутри элемента textarea
   textarea.select();
-  
+
   // Копируем выделенный текст в буфер обмена
   document.execCommand("copy");
-  
+
   // Удаляем временный элемент textarea
   document.body.removeChild(textarea);
-  
+
   // Добавляем обратную связь или уведомление о том, что текст скопирован
-  const tooltipText = button.querySelector('.copy-message');
-  tooltipText.style.visibility = 'visible';
-  
+  const tooltipText = button.querySelector(".copy-message");
+  tooltipText.style.visibility = "visible";
+
   // Скрываем подсказку через некоторое время
   setTimeout(() => {
-      tooltipText.style.visibility = 'hidden';
-  }, 2000);  // 2 секунды
+    tooltipText.style.visibility = "hidden";
+  }, 2000); // 2 секунды
 }
 
 // Назначаем обработчик события на все кнопки с классом copy-button
-const copyButtons = document.querySelectorAll('.copy-button');
-copyButtons.forEach(button => {
-  button.addEventListener('click', copyText);
+const copyButtons = document.querySelectorAll(".copy-button");
+copyButtons.forEach((button) => {
+  button.addEventListener("click", copyText);
 });
